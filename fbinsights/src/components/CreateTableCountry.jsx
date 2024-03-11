e6b4d4f1e13746375d6233bd539fcea00759d935
Ayush Katoch <ayush98katoch@gmail.com>
2024-03-12 01:33:28 +0530

    initial commit

--- fbinsights/src/components/CreateTableCountry.jsx
+++ fbinsights/src/components/CreateTableCountry.jsx
@@ -2,14 +2,47 @@
 import React from 'react';
 import { extractDateFromISOString } from "../utility.js";
 
+function processData(data){
+
+  const finalData = {}
+
+  // get list of all the countries 
+
+  for(const key in data[data.length - 1].value){
+      finalData[key] = []
+  }
+  
+  for(let i = data.length - 1; i >= 0; i--){
+
+    const record = data[i];
+    let lastEndTime = "";
+    for(const key in finalData){
+      
+      if (key in record.value) {
+        finalData[key].push({end_time: record.end_time, val: record.value[key]});
+        lastEndTime = record.end_time
+      }
+      else{
+        finalData[key].push({end_time: lastEndTime, val: 0});
+      }
+
+    }
+
+
+  }
+
+  return finalData;
+
+}
+
 export function CreateTableCountry({content, title}) {
     if (content == null || content.length == 0) return <> </>
     
-    const arr = []
+    const processedData = processData(content);
 
-
+  console.log("prcessedData", processedData);
     const renderTableHeader = () => {
-        const header = Object.keys(content[0].value);
+        const header = Object.keys(processedData);
         return (
             <>
             
@@ -19,8 +52,8 @@ export function CreateTableCountry({content, title}) {
             </tr>
             <tr>
                 <th className="py-2 px-4 border-b">End Time</th>
-                    {header.map((region) => (
-                    <th className="py-2 px-4 border-b" key={region}>{region}</th>
+                    {header.map((key) => (
+                    <th className="py-2 px-4 border-b" key={key}>{key}</th>
                 ))}
             </tr>
             </>
@@ -28,14 +61,29 @@ export function CreateTableCountry({content, title}) {
       };
     
       const renderTableRows = () => {
-        return content.map((entry) => (
-          <tr key={entry.end_time}>
-            <td className="py-2 px-4 border-b">{extractDateFromISOString(entry.end_time)}</td>
-            {Object.values(entry.value).map((value, index) => (
-              <td className="py-2 px-4 border-b" key={index}>{value}</td>
-            ))}
-          </tr>
-        ));
+        const allKeys = Object.keys(processedData);
+        let output = [];
+      
+        for (let i = 0; i < processedData[allKeys[0]].length; i++) {
+          // output.push(<tr key={i}>);
+          let runOnce = true;
+          let temp = []
+          for (const key of allKeys) {
+            if (runOnce)
+            {
+              const d = extractDateFromISOString(processedData[key][i].end_time);    
+              temp.push(<td key={key + i + "1"} className="py-2 px-4 border-b">{extractDateFromISOString(processedData[key][i].end_time)}</td>);
+              runOnce = false;
+            }
+            temp.push(<td key={key + i + "2"} className="py-2 px-4 border-b">{processedData[key][i].val}</td>);
+          }
+
+          output.push(<tr className="py-2 px-4 border-b">{temp}</tr>)
+      
+      {/* output.push(</tr>); */}
+        }
+      
+        return output;
       };
     
     
